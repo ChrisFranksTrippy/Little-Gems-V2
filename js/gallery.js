2258427c596668a9069c8f77d5ac30a33dc34ab3
Chris Franks <1000frankschris@gmail.com>
2017-11-23 21:04:39 +0000

    Page created
    Layout designed
    JavaScript being implemented

---
+++ js/gallery.js
@@ -0,0 +1,10 @@
+(function(){
+  
+  let slideContainer = document.getElementById("slider-container");
+  
+  console.log("Hello");
+  console.log(slideContainer.offsetWidth);
+  console.log(slideContainer.childElementCount);
+  console.log(slideContainer.children[0].offsetWidth);
+  
+})();
