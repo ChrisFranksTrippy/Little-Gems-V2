781218c9d0dbae004b09c84336900bb9745f8ced
Chris Franks <1000frankschris@gmail.com>
2017-12-03 14:18:03 +0000

    Bug fix
    Tested local, scroll-arrow.js html.scrollTop value was not changing on
    mobile devices. The chrome simulator gave false results. Solution was to
    use the body tab rather than html

--- js/scroll-arrow.js
+++ js/scroll-arrow.js
@@ -1,10 +1,10 @@
 (function() {
   
-  let html = document.getElementsByTagName("html")[0];
+  let body = document.getElementsByTagName("body")[0];
   let windowHeight = window.innerHeight;
   let topArrow = document.getElementById("top-arrow");
   
-  console.log(html);
+  console.log(body);
   
   document.addEventListener("scroll", toggleScrollArrow);
   document.addEventListener("onresize", function() { 
@@ -16,9 +16,9 @@
   
   function toggleScrollArrow() {
     
-    //console.log(html.scrollTop);
+    console.log(body.scrollTop);
     
-    if (html.scrollTop > (window.innerHeight / 2)) {
+    if (body.scrollTop > (window.innerHeight / 2)) {
       //console.log(window.innerHeight);
       topArrow.classList.add("show-arrow");
     } else {
