065edcf63169e66f282339605a60c4aab99e9d2f
Chris Franks <1000frankschris@gmail.com>
2017-12-20 23:08:04 +0000

    HTML semantic tags
    Small bug fixes and tag correction

--- js/dog-api.js
+++ js/dog-api.js
@@ -17,20 +17,23 @@
 
 	let request = new Request(breedListUrl);
 
+	//Get list of breads
 	fetch(request)
 		.then(function (response) {
-			console.log(`response: ${response.status}`);
+			//console.log(`response: ${response.status}`);
 			return response.json();
 		})
 		.then(function (data) {
-			console.log(data);
+			//console.log(data);
 			let key;
 			let theData = "";
 			breedListArray = data.message;
+			//input json data to list of datalist options
 			for (key in breedListArray) {
 				theData += `<option value="${breedListArray[key]}">`;
 			}
 
+			//input options into data list
 			breedList.innerHTML = theData;
 
 			console.log(breedListArray);
@@ -39,42 +42,54 @@
 
 	checkBtn.addEventListener("click", fetchImg);
 
-	
-	
+
+
 
 	function fetchImg() {
-		
-		console.log("fetch img!");
+
+		////console.log("fetch img!");
 		console.log(breedInput.value);
-		
+
 		let key;
-		let inputTxt = breedInput.value.toLowerCase();
-		
+		//Remove white space and change to lower case
+		let inputTxt = breedInput.value.toLowerCase().trim();
+
 		for (key in breedListArray) {
 			if (inputTxt === breedListArray[key]) {
 				//Match
-				console.log("match!");
-				
-				let request = new Request(imgListUrlStart + breedListArray[key] + imgListUrlEnd); 
+				//console.log("match!");
+
+				//Get Rnd Image
+				let request = new Request(imgListUrlStart + breedListArray[key] + imgListUrlEnd);
 
 				fetch(request)
 					.then(function (response) {
-						console.log(`response: ${response.status}`);
+						//console.log(`response: ${response.status}`);
 						return response.json();
 					})
 					.then(function (data) {
-						console.log(data);
-						
+						//console.log(data);
+
+						//remove img size class
 						if (breedImg.classList.contains("unknown-breed")) {
 							breedImg.classList.remove("unknown-breed");
-						}				
-					
+						}
+
+						//Change img src
 						breedImg.src = data.message;
+						//Add caption text
 						breedCaption.innerHTML = breedListArray[key];
-					
+
 					});
 
 				break;
+			} else {
+				//Add unknow image
+				if (!breedImg.classList.contains("unknown-breed")) {
+					breedImg.classList.add("unknown-breed");
+					breedImg.src = "img/Unknown.png";
+					breedCaption.innerHTML = "";
+				}
 			}
 		}
 	}
